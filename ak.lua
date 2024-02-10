4437b918f847cd92684059626e164dd8562807df
akki697222 <151368291+akki697222@users.noreply.github.com>
2024-02-10 16:17:39 +0900

    Update ak.lua

--- ak.lua
+++ ak.lua
@@ -4,15 +4,16 @@ function updatehelp()
     print("help:")
 end
 
-function term_YorN(string)
+function term_YorN(str)
+    local message = str.tostring
     term.clear()
     term.setCursorPos(1,0)
-    print(string)
+    print(message)
     print("Y or N")
     local input = read()
-    if input == "y" or input == "Y" then
+    if string.find(input, "y") or string.find(input, "Y") then
         return true 
-    elseif input == "n" or input == "N" then
+    elseif string.find(input, "n") or string.find(input, "N") then
         return false
     else
         print("Please enter y or n")
@@ -20,7 +21,7 @@ function term_YorN(string)
 end
 
 function update(programName)
-    shell.run(string.format("wget https://raw.githubusercontent.com/akki697222/akkiserver-computercraft-script/main/updatable/%d.lua", programName))
+    shell.run("wget https://raw.githubusercontent.com/akki697222/akkiserver-computercraft-script/main/updatable/",programName,".lua")
     print("Update Completed!!")
 end
 
@@ -30,7 +31,8 @@ if args[1] == "update" then
         print("please type program name")
     else
         local pn = args[2]
-        local bool = term_YorN("Do you want to delete old files?")
+        print("Do you want to delete old files?")
+        local bool = term_YorN()
         if bool == true then
             shell.run(string.format("rm %d.lua", pn))
             update(pn)
